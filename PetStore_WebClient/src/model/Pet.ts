1aba0c2a567d0c7b934f7c0c4e7d1b5c52d2cfd8
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-12 18:13:23 +0200

    the price of the pet is shown

--- PetStore_WebClient/src/model/Pet.ts
+++ PetStore_WebClient/src/model/Pet.ts
@@ -5,19 +5,21 @@ export class Pet {
     public animalType: number;
     public birthDate: Date;
     public sex: boolean;
+    public price: Number;
 
     /**
      *
      */
     constructor(Id: Number,
         name: string, description: string, animalType: number,
-        birthDate: Date, sex: boolean) {
+        birthDate: Date, sex: boolean, price: Number) {
         this.id = Id;
         this.name = name;
         this.description = description;
         this.animalType = animalType;
         this.birthDate = birthDate;
         this.sex = sex;
+        this.price = price;
     }
 
 
