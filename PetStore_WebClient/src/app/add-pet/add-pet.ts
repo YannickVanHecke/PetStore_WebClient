6e7422e0174ea1353580735cba3503cac123b154
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-13 10:04:04 +0200

    created new component for adding new pet into the petstore.

---
+++ PetStore_WebClient/src/app/add-pet/add-pet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pet',
+  standalone: false,
+  templateUrl: './add-pet.html',
+  styleUrl: './add-pet.scss'
+})
+export class AddPet {
+
+}
--- PetStore_WebClient/src/app/app-module.ts
+++ PetStore_WebClient/src/app/app-module.ts
@@ -8,6 +8,7 @@ import { SexPipe } from '../pipes/sex-pipe';
 import { AnimalTypePipe } from '../pipes/animal-type-pipe';
 import { provideHttpClient } from '@angular/common/http';
 import { AgePipe } from '../pipes/age-pipe';
+import { AddPet } from './add-pet/add-pet';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { AgePipe } from '../pipes/age-pipe';
     AgePipe,
     AnimalTypePipe,
     SexPipe,
+    AddPet,
   ],
   imports: [
     BrowserModule,
