3e1000dd17c7579dea2c55ccfb55be388fae7f5d
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-13 10:11:41 +0200

    making routing to addpet and petstore-overview works.

--- PetStore_WebClient/src/app/app-routing-module.ts
+++ PetStore_WebClient/src/app/app-routing-module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PetStoreOverview } from './pet-store-overview/pet-store-overview';
+import { AddPet } from './add-pet/add-pet';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '', redirectTo: '/petstore', pathMatch: 'full' }, // Default route
   { path: 'petstore', component: PetStoreOverview },
+  { path: 'petstore/new', component: AddPet},
   { path: '**', component: PetStoreOverview }
 ];
 
