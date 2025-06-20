58740408d97b9ea714fe3370d84fb244eafdee91
yannickvanhecke <yannick.vanhecke@icloud.com>
2025-06-20 15:43:26 +0200

    Revert "showing hard coded chart works."
    This reverts commit 937d91abb1ace797d520c8c28370a8dba74fde6c.

--- PetStore_WebClient/src/app/statistics/statistics.ts
+++ PetStore_WebClient/src/app/statistics/statistics.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
@@ -7,12 +7,5 @@ import { Component, ViewChild } from '@angular/core';
   styleUrl: './statistics.scss'
 })
 export class Statistics {
-  public chartData = [112, 10, 225, 134, 101, 80, 50, 100, 200];
-  public labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'];
-
-  public tooltipContent = '';
-  public tooltipOpen = false;
-  public tooltipX = 0;
-  public tooltipY = 0;
 
 }
